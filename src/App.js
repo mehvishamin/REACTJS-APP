0a8f39c7b2ebdfd1d089197b27a098e42efcaebb
mehvish <mehvishamin99@gmail.com>
2024-03-08 00:20:56 +0530

    MY FIRST COMMIT

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Practice from "./Practice";
+
+function App() {
+  return (
+    <>
+    <Practice/>
+    <h2>This is the first create app project</h2>
+  </> 
+  )
+
+}
+
+export default App;
---
+++ src/Practice.js
@@ -0,0 +1,6 @@
+const Practice=(()=>{
+    return(
+        <h3>First Practice</h3>
+    )
+})
+export default Practice
